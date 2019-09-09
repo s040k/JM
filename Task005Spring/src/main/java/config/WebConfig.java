e7336732becc43d426bf72974b28422b0d2508ed
s040k <s040k@mail.ru>
2019-09-09 09:03:42 +0300

    Рабочая версия 5 задние вторая правка 9 сентября

---
+++ Task005Spring/src/main/java/config/WebConfig.java
@@ -0,0 +1,31 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"config","controller","service","model","DAO"})
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Bean
+    public ViewResolver getViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setOrder(1);
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setPrefix("/WEB-INF/pages/");
+
+        return viewResolver;
+    }
+}
