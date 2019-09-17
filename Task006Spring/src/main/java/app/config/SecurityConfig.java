e1ea61b6301a679d9ba87d3d2c69af353538fd48
s040k <s040k@mail.ru>
2019-09-17 12:03:22 +0300

    Первые правки 6-е задание 16,09

--- Task006Spring/src/main/java/app/config/SecurityConfig.java
+++ Task006Spring/src/main/java/app/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package app.config;
 
-import app.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
